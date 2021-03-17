19ea90d5c187f15e03c8524b19ea3fcfa754b247
davipadua <davipadua1@gmail.com>
2021-03-17 18:42:50 -0300

    novas alteracoes

---
+++ src/main/java/lista02_exercicio01/Candidato.java
@@ -0,0 +1,24 @@
+package lista02_exercicio01;
+
+public class Candidato {
+	
+	public Candidato(int id, String nome) {
+		
+		this.id = id;
+		this.nome = nome;
+	}
+	
+
+	private int id;
+	private String nome;
+
+	public int getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+
+}
--- src/main/java/lista02_exercicio01/Main.java
+++ src/main/java/lista02_exercicio01/Main.java
@@ -12,10 +12,79 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 		Main app = new Main();
-		app.start();
+//		app.start();
+		app.resposta();
 
 	}
+	
+	private Map<Integer, Candidato> mapaDeCandidatos = new HashMap<Integer, Candidato>();
 
+	private void resposta() {
+		// TODO Auto-generated method stub
+		
+		inicializarMapaDeCandidatos();
+		
+		while(true) {
+			
+			int voto = obterVoto();
+			if(voto == 0) {
+				break;
+			}
+			
+			System.out.println("Numero do voto " + voto);
+			
+		}
+		
+		System.out.println("Fim da aplicação");
+		
+
+	}
+	
+	private void inicializarMapaDeCandidatos() {
+		
+		Candidato joao = new Candidato(1, "João");
+		mapaDeCandidatos.put(joao.getId(), joao);
+		
+		Candidato maria = new Candidato(2, "Maria");
+		mapaDeCandidatos.put(maria.getId(), maria);
+		
+		Candidato jose = new Candidato(3, "José");
+		mapaDeCandidatos.put(jose.getId(), jose);
+		
+		Candidato antonio = new Candidato(4, "Antonio");
+		mapaDeCandidatos.put(antonio.getId(), antonio);
+		
+		Candidato tiburssinho = new Candidato(5, "Tiburssinho");
+		mapaDeCandidatos.put(tiburssinho.getId(), tiburssinho);
+		
+	}
+
+	private int obterVoto() {
+		
+		System.out.println("Digite seu voto: ");
+		Scanner scanner = new Scanner(System.in);
+		
+		try {
+			
+			return scanner.nextInt();
+
+		} catch (Exception e) {
+			
+			scanner.next();
+			System.out.println("Valor inválido");
+			return -1;
+			
+		}
+		
+	}
+	
+	private void registrarVoto(int voto) {
+		
+		/// NOME_MAPA.containsKey(CHAVE_EM_QUESTAO)
+		/// NOME_MAPA.get(CHAVE_EM_QUESTAO)
+		
+	}
+	
 	private void start() {
 		// TODO Auto-generated method stub
 		
